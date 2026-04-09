6e3d0eca65811f35b24ee513807b05bff62df0ad
DanielPot217 <danielp0517@gmail.com>
2026-04-09 08:40:18 -0400

    Tweaked DB Config for AWS

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'clt_squared',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Event listeners for pool
